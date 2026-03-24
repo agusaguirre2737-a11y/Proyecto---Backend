3c752d0ba0898ff5f2622a7ec4855e59e8cc2961
agusaguirre2737-a11y <AgusAguirre2737@gmail.com>
2026-03-24 02:56:08 -0300

    Etapa 2 Completa

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import express from "express";
+import categoriasmockRouter from "./routes/categoriasmock.js";
 
 // crear servidor
 const app = express();
+
+//middleware para poder leer el json del post
+app.use(express.json());
 
 // controlar ruta
 app.get("/", (req, res) => {
@@ -16,3 +20,8 @@ app.locals.fechaInicio = new Date();
 app.listen(port, () => {
 console.log(`sitio escuchando en el puerto ${port}`);
 });
+
+//Una vez definido el controlador de nuestro recurso debemos vincularlo a nuestra aplicación
+//express, cargando el módulo de ruta en el archivo index.js antes de levantar el servidor
+//(app.listen...)
+app.use(categoriasmockRouter);
